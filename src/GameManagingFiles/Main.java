6d33ec64952ce10c48c160347f5a91c64a6c6568
That_One_Catboy <jadecook636@gmail.com>
2023-04-17 08:18:59 -0700

    oipiwaeufhafhawfephiawef

--- src/GameManagingFiles/Main.java
+++ src/GameManagingFiles/Main.java
@@ -3,9 +3,13 @@ package GameManagingFiles;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
-        for (int i = 0; i < 10; i++) {
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
             System.out.println(i);
             MapGenerator map = new MapGenerator(10, 10);
         }
+        long time2 = System.currentTimeMillis();
+        long time3 = time2 - time;
+        System.out.println(time3);
     }
 }
--- src/OtherFiles/Quest.java
+++ src/OtherFiles/Quest.java
@@ -2,5 +2,6 @@ package OtherFiles;
 
 public class Quest {
     // private final NPC deliverTo;
-
+    private final int goldReward;
+    // private final
 }
