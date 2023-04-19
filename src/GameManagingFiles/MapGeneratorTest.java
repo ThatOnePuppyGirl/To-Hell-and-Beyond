21680b8ad74a133daa41a1d61091f42a7a70945d
That_One_Catboy <jadecook636@gmail.com>
2023-04-19 09:57:58 -0700

    updated tests to reflect current requirements

--- src/GameManagingFiles/MapGeneratorTest.java
+++ src/GameManagingFiles/MapGeneratorTest.java
@@ -15,12 +15,22 @@ public class MapGeneratorTest {
     @Test
     public void TestMapGeneratorMultiple() {
         int count = 0;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100000; i++) {
             System.out.println(i);
             MapGenerator map = new MapGenerator(10, 10);
             if (map.IsMapInvalid())
                 count++;
         }
         assertEquals(count, 0);
+    }
+
+    @Test
+    public void TestMapGeneratorLongestPath() {
+        MapGenerator map = new MapGenerator(10, 10);
+        int[] w = map.FindLongestConnections();
+        System.out.println(w[0] + " x: " + w[1] + " y: " + w[2]);
+        assertTrue((w[0] <= 100));
+        assertTrue(((w[1] >= 0) && (w[1] <= 10)));
+        assertTrue(((w[2] >= 0) && (w[2] <= 10)));
     }
 }
